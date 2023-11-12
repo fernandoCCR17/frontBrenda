c5c2b38bb96f774e6b78145f1b9b1686f0791a9b
Fernando De la Cruz Ramos <fernandodelacruzramos2001@gmail.com>
2023-11-11 22:02:04 -0600

    cambio url fetch

--- js/app.js
+++ js/app.js
@@ -1,8 +1,8 @@
-let estaLogueado = false, cliente;//variable que nos permite verificar si un usuario esta logueado y otra para almacenar los datos del usuario 
+let estaLogueado = false, cliente; //variable que nos permite verificar si un usuario esta logueado y otra para almacenar los datos del usuario 
 (async function(){
     try {
-        //hacemos un fetch a la url para traer los datos del usuario y si este 
-        const respuesta = await fetch(`http://localhost:3000/auth/datos_usuario/${localStorage.getItem("token") || "singup"}`, {
+        //hacemos un fetch a la url para traer los datos del usuario y si este logueado
+        const respuesta = await fetch(`http://34.16.180.179/app/auth/datos_usuario/${localStorage.getItem("token") || "singup"}`, {
             method: "GET", // El método de la solicitud (puede ser GET, POST, etc.)
             headers: {
               "Authorization": `Bearer ${localStorage.getItem("token") || "singup"}` // Agrega el token en el encabezado Authorization
@@ -178,7 +178,7 @@ function iniciarApp(){
 
     async function enviaPeticion(datos){
         try{
-            const resultado = await fetch("http://localhost:3000/auth/crearReserva", {
+            const resultado = await fetch("http://34.16.180.179/app/auth/crearReserva", {
                 method: "POST",
                 headers: {
                     "Content-Type": "application/json"
--- js/appLogin.js
+++ js/appLogin.js
@@ -103,7 +103,7 @@ function iniciarApp(){
     async function enviaPeticion(datos, tipo){
         console.log(datos)
         try{
-            const resultado = await fetch(`http://localhost:3000/auth/${tipo}`, {
+            const resultado = await fetch(`http://34.16.180.179/app/auth/${tipo}`, {
                 method: "POST",
                 headers: {
                     "Content-Type": "application/json"
